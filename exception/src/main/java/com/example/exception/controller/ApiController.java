b93eb2f86eee19ae9578290915f2595d193cee30
MWJOB <okkilltogo@gmail.com>
2024-02-26 22:16:58 +0900

    exception 실습

--- exception/src/main/java/com/example/exception/controller/ApiController.java
+++ exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -1,2 +1,26 @@
-package com.example.exception.controller;public class ApiController {
+package com.example.exception.controller;
+
+
+import com.example.exception.dto.User;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping()
+    public User get(@RequestParam String name, @RequestParam int age){
+        User user = new User();
+        user.setAge(age);
+        user.setName(name);
+
+        return user;
+    }
+
+    @PostMapping("")
+    public User post(@Valid @RequestBody User user){
+        System.out.println(user);
+        return user;
+    }
 }
--- exception/src/main/java/com/example/exception/dto/User.java
+++ exception/src/main/java/com/example/exception/dto/User.java
@@ -1,2 +1,29 @@
-package com.example.exception.dto;public class user {
+package com.example.exception.dto;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    @NotEmpty
+    @Size(min = 1, max = 10)
+    private String name;
+
+    @Min(1)
+    @NotNull
+    private int age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
