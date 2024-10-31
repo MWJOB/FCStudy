7e980210c6f9d9059645e34cda1129e045f95731
MWJOB <okkilltogo@gmail.com>
2024-10-31 22:47:03 +0900

    테스트

---
+++ spring_JPA/spric-sec/src/main/java/org/example/spricsec/SpricSecApplication.java
@@ -0,0 +1,13 @@
+package org.example.spricsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpricSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpricSecApplication.class, args);
+    }
+
+}
---
+++ spring_JPA/spric-sec/src/test/java/org/example/spricsec/SpricSecApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spricsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpricSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
