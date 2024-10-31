9b42d2856fcc34d68b8eea55d7d4cd8cb41d8c16
MWJOB <okkilltogo@gmail.com>
2024-10-31 18:53:27 +0900

    Service 구현

--- naver_api_restaurant/src/test/java/org/example/naver_api_restaurant/naver/NaverClientTest.java
+++ naver_api_restaurant/src/test/java/org/example/naver_api_restaurant/naver/NaverClientTest.java
@@ -2,6 +2,7 @@ package org.example.naver_api_restaurant.naver;
 
 import org.example.naver_api_restaurant.naver.dto.SearchImageReq;
 import org.example.naver_api_restaurant.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,7 @@ public class NaverClientTest {
 
         var result = naverClient.searchLocal(search);
         System.out.println(result);
+        Assertions.assertNotNull(result.getItems().stream().findFirst().get().getCategory()); //꺠지기 쉬운 테스트 코드
     }
 
     @Test
